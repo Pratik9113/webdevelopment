026717ff432dc0b09ce5e1c288777b30985eb000
Pratik Patil <pratik.patil9113@gmail.com>
2023-11-22 19:16:29 +0530

    js lec2 operation and condition

---
+++ js/js_tutorial/lec2/script.js
@@ -0,0 +1,74 @@
+// // this is comment 
+// /* this is again a comment */
+// console.log("hello world");
+// let a = 5;
+// let b = 2;
+// // let c = a+b;
+// // console.log(c);
+// console.log("a+b : ", a+b);
+// console.log("a-b : ", a-b);
+// console.log("a*b : ", a*b);
+// console.log("a/b : ", a/b);
+// console.log("a%b : ", a%b);
+// console.log("a**b : ", a**b);
+// console.log("operation in js ");
+// a+=4;
+// console.log(a);
+// let a = 5;
+// let b = "5";
+// console.log(a!=b);
+// console.log(a ==b);
+// console.log(a===b);
+// console.log(a=b);
+// console.log(a===b);
+// console.log(a!==b);
+
+
+
+// logical operation ...........
+let a = 6;
+let b = "5";
+
+// let cond1 = a>b;
+// let cond2 = a===b;
+// console.log(cond1&&cond2);
+// console.log(cond1||cond2);
+// console.log(! cond2);
+
+// conditional statement ........
+
+// let age = 1;
+
+// if(age > 18){
+//     console.log("you can go");
+// }else{
+//     console.log("you can't go");
+// }
+
+// let mode = "dark";
+// let color;
+// if(mode === "dark"){
+//     color = "black";
+// }else{
+//     color = "white";
+// }
+// console.log(color);
+
+// if(mode == "dark") console.log(mode);
+
+// let result = age > 18 ? "adult" : "not adult";
+// console.log(result);
+
+
+// pratice 1
+// alert("hello");
+// prompt can take input 
+// let fullname = prompt("hello");
+// console.log(fullname);
+
+let num = prompt("enter a number : ");
+if(num%5===0){
+    console.log("yes it is multiple of 5");
+}else{
+    console.log("ahh no  it is not  multiple of 5")
+}
