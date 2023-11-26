3fd14013d52d7989e54f3a400236f78bcd080b50
Pratik Patil <pratik.patil9113@gmail.com>
2023-11-26 22:01:49 +0530

    loop and string

---
+++ js/js_tutorial/lec3-loop_string/loop_string.js
@@ -0,0 +1,31 @@
+// let str = "pratik singh";
+// for(let i of str){
+//     console.log(i);
+// }
+// // for in loop 
+// let student = {
+//     name : "pratik singh",
+//     age : 20,
+//     cgpa : 8.89,
+//     div : "D10C"
+// };
+// for(let i in student){
+//     console.log( i + " : "+  student[i]);
+// }
+
+
+// let create a game 
+// there is a number and we have to enter that number by guessing 
+// let lotteryNum = 79;
+// let userNum = prompt("guess the lottery number ");
+
+// while(userNum != lotteryNum){
+//     userNum = prompt("enter again this is wrong lottery number ");
+// }
+
+// console.log("congralution , you won the lottery ");
+
+// string 
+let fullname = prompt("enter your name : ");
+let username = "@" + fullname + fullname.length;
+console.log(username);
