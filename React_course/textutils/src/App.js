c9d48ba38941724fbe835df48065c9150a240265
Pratik Patil <pratik.patil9113@gmail.com>
2023-12-17 22:44:13 +0530

    more about react

---
+++ React_course/textutils/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+let name = "pratik";
+function App() {
+  return (
+    <>
+      <div className="blank">
+        pratik
+        <h1>hello {name} </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
