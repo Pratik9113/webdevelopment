0c9fdfbcc3bc60e7161a8a35a844e245b4fd0737
Pratik Patil <pratik.patil9113@gmail.com>
2023-12-18 20:47:38 +0530

    nodejs -path os

---
+++ NodeJs/nodejs_tutorial/importExport/index.js
@@ -0,0 +1,19 @@
+// // const oper = require('./oper.js');
+// // // console.log(add(5,5));
+// // console.log(oper)
+// // console.log(oper.add(5,5))
+// // console.log(oper.sub(5,5))
+
+
+// const {add,sub,mul} = require('./oper.js');
+// console.log(add(5,5));
+// console.log(sub(5,5));
+// console.log(mul(5,5));
+
+
+// index.js
+const { add, sub, mul } = require('./oper.js');
+
+console.log(add(5, 5));
+console.log(sub(5, 5));
+console.log(mul(5, 5));
