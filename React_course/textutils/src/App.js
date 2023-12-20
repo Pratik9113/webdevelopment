9e59f02c09f82a60cc1235c492b77b0e08688296
Pratik Patil <pratik.patil9113@gmail.com>
2023-12-20 17:16:49 +0530

    textutils

--- React_course/textutils/src/App.js
+++ React_course/textutils/src/App.js
@@ -1,16 +1,25 @@
+import React,{ useState } from 'react';
 import './App.css';
 import About from './components/About';
 import Navabar from './components/Navabar';
 import Tech from './components/Tech';
 function App() {
+  const [mode,setMode] = useState('light'); // where is my dark mode is true or false
+  const toggleMode = () => {
+    if(mode==='light'){
+      setMode('black');
+    }else{
+      setMode('light');
+    }
+  }
   return (
     <>
     {/* this tiltle is prop from here i can change the tittle */}
-      <Navabar title = "TextUtils" aboutText = "about-textutils"/>
+      <Navabar title = "TextUtils" aboutText = "about-textutils" mode={mode} toggleMode = {toggleMode}/>
       <Tech/>
       <div className="container my-3">
           {/* <Tech heading = "Words counter "/> */}
-          <About/>
+          
       </div>
     </>
   );
--- React_course/textutils/src/components/Navabar.js
+++ React_course/textutils/src/components/Navabar.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const Navabar = (props) => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
         <div className="container-fluid">
           <a className="navbar-brand" href="/">
             {props.title}
@@ -21,7 +21,7 @@ const Navabar = (props) => {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul className={`navbar-nav me-auto mb-2 mb-lg-0 text-${props.mode === 'light' ? 'dark' : 'light'}`}>
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="/">
                   {props.aboutText}
@@ -69,17 +69,10 @@ const Navabar = (props) => {
                 </a>
               </li>
             </ul>
-            <form className="d-flex" role="search">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
+            <div className={`form-check-label text-${props.mode === 'light' ? 'dark' : 'light'}`}>
+                <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+                <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Dark Mode</label>
+            </div>
           </div>
         </div>
       </nav>
