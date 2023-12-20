ec6c8def1fa7d06b5f9c5b8a165a752f35effb0f
Pratik Patil <pratik.patil9113@gmail.com>
2023-12-20 16:38:39 +0530

    dark mode in react

--- React_course/textutils/src/App.js
+++ React_course/textutils/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import About from './components/About';
 import Navabar from './components/Navabar';
 import Tech from './components/Tech';
 function App() {
@@ -6,8 +7,10 @@ function App() {
     <>
     {/* this tiltle is prop from here i can change the tittle */}
       <Navabar title = "TextUtils" aboutText = "about-textutils"/>
+      <Tech/>
       <div className="container my-3">
-          <Tech heading = "enter the test to anlayze below"/>
+          {/* <Tech heading = "Words counter "/> */}
+          <About/>
       </div>
     </>
   );
--- React_course/textutils/src/components/Tech.js
+++ React_course/textutils/src/components/Tech.js
@@ -11,12 +11,41 @@ const Tech = (props) => {
     setText(newtext);
   }
 
+  const handleClearCase = () =>{
+    console.log("text is cleared" + text);
+    let newtext = '';
+
+    setText(newtext);
+  }
+
+
+  const handleCopytext = () => {
+    console.log("copy text");
+    var text = document.getElementById("mybox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  }
+
+  const handleLowerCase = () =>{
+    console.log("Lowercase is clicked" + text);
+    let newtext = text.toLowerCase();
+
+    setText(newtext);
+  }
+
+
+  const handleExtraSpaces = () => {
+    console.log("remove extra space");
+    var newtext = text.replace(/\s+/g, ' ');
+    setText(newtext);
+  }
   const handleOnChange = (event) =>{
     console.log("on change is happpening ")
     setText(event.target.value)
   }
   return (
     <>
+    <div className="container">
     <div>
         <h1>{props.heading}</h1>
         <div className="mb-3">
@@ -24,7 +53,19 @@ const Tech = (props) => {
             <textarea className="form-control"value= {text} onChange={handleOnChange} id="mybox" rows="8"></textarea>
         </div>
 
-        <button className='btn btn-primary' onClick={handleUpperCase}>convert to uppercase</button>
+        <button className='btn btn-primary mx-2' onClick={handleUpperCase}>convert to uppercase</button>
+        <button className='btn btn-primary mx-2' onClick={handleLowerCase}>convert to lowercase</button>
+        <button className='btn btn-primary mx-2' onClick={handleClearCase}>clear</button>
+        <button className='btn btn-primary mx-2' onClick={handleCopytext}>copy</button>
+        <button className='btn btn-primary mx-2' onClick={handleExtraSpaces}>remove spaces</button>
+    </div>
+    </div>
+    <div className="container my-2">
+      <h1>your text summary</h1>
+      <p> {text.split(" ").length} words and  {text.length} characters</p>
+      <p>user can read in time  {0.008 * text.split(" ").length}</p>
+      <h2>preview</h2>
+      <p>{text}</p>
     </div>
     </>
   )
