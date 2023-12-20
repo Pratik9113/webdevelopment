be51c468b5076fd58239ec7f1041bcc65bd8e3cf
Pratik Patil <pratik.patil9113@gmail.com>
2023-12-21 01:01:25 +0530

    validator for email correctedness and all function package

---
+++ NodeJs/nodejs_tutorial/4npmMod/index.mjs
@@ -0,0 +1,6 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('Hello world!'));
+import validator from 'validator';
+const result = validator.isEmail('pratik.patil113gmail.com');
+console.log(result ? chalk.green.inverse(result) : chalk.red.inverse(result));
