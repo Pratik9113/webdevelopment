51b7099221fed72e4a13c452775b6b1d1fc3a469
Pratik Patil <pratik.patil9113@gmail.com>
2023-12-21 01:18:25 +0530

    nodemon

--- NodeJs/nodejs_tutorial/4npmMod/index.mjs
+++ NodeJs/nodejs_tutorial/4npmMod/index.mjs
@@ -2,5 +2,5 @@ import chalk from 'chalk';
 
 console.log(chalk.blue('Hello world!'));
 import validator from 'validator';
-const result = validator.isEmail('pratik.patil113gmail.com');
+const result = validator.isEmail('pratik.patil113@gmail.com');
 console.log(result ? chalk.green.inverse(result) : chalk.red.inverse(result));
