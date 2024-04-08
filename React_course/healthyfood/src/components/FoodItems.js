3292d775c6792f831ef43cae08b815bf2b653bb6
Pratik9113 <117252760+Pratik9113@users.noreply.github.com>
2024-04-08 21:07:14 +0530

    managning statev

--- React_course/healthyfood/src/components/FoodItems.js
+++ React_course/healthyfood/src/components/FoodItems.js
@@ -3,14 +3,17 @@ import Item from "./Item";
 const FoodItems = ({items}) =>{
 
     let [activeItem,setActiveItem] = useState([]);
+    let onBuyButton = (item ,event) =>{
+        let newItems =  [...activeItem,item]
+        setActiveItem(newItems)
+    }
     return (
         
         <ul className = "list-group">
             {items.map((item)=>(
                 <Item key = {item} foodItem = {item} bought = {activeItem.includes(item)}
-                handleBuyButton = {()=> console.log(` ${item} is clicked `)}
+                handleBuyButton = {(event)=> onBuyButton(item,event)}
                 />
-
         ))}
         </ul>
         
