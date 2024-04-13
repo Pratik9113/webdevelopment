10015d8ea5aa013f0643fd0bdce0574ad366ed05
Pratik9113 <117252760+Pratik9113@users.noreply.github.com>
2024-04-13 09:54:14 +0530

    about form

--- React_course/todo1/src/components/AddTodo.js
+++ React_course/todo1/src/components/AddTodo.js
@@ -1,39 +1,48 @@
-import { useState } from "react";
-import TodoItem from "./TodoItem";
-
+import { useState ,useRef} from "react";
 function AddTodo({onNewItem}) {
     const [todoName,setTodoName ] = useState()
     const [dueDate,setDueDate] = useState() 
+    const noOfUpdates = useRef(0)
+
 
     const handleNameChange = (event) => {
         setTodoName(event.target.value)
+        noOfUpdates.current += 1;
     }
     const handleDateChange = ( event) =>{
         setDueDate(event.target.value)
+        console.log(`no of updates are : ${noOfUpdates.current}`)
     }
 
 
     // state declare kiya then use hum add button click hone ke baad usko parent ke pass bhejenge then empty string karenge
-    const handleAddButtonClicked = () =>{
+    const handleAddButtonClicked = (event) =>{
+        console.log(event)
+        event.preventDefault();
         onNewItem(todoName,dueDate)
         setDueDate("")
         setTodoName("")
-    }
+        // preventDefault 
+   }
     return (
         <div className="container">
-            <div className="row kg-row">
-            <div className="col-6">
-                <input type="text" value = {todoName} placeholder="Enter Todo  Here" onChange={handleNameChange}/>
-            </div>
-            <div className="col-4">
-                <input type="date" value = {dueDate} onChange={handleDateChange}/>
-            </div>
-            <div className="col-2">
-                <button type="button" className="btn btn-success kg-button"
-                onClick={handleAddButtonClicked}
-                >Add</button>
-            </div> 
-            </div>
+            <form  className="row kg-row"
+            onSubmit={handleAddButtonClicked}
+            >
+            
+                <div className="col-6">
+                    <input type="text" value = {todoName} placeholder="Enter Todo  Here" onChange={handleNameChange}/>
+                </div>
+                <div className="col-4">
+                    <input type="date" value = {dueDate} onChange={handleDateChange}/>
+                </div>
+                <div className="col-2">
+                    <button 
+                    type = "submit"
+                    className="btn btn-success kg-button"
+                    >Add</button>
+                </div> 
+            </form>
         </div>
     )
 }
