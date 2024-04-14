ec3f012da9a96ab9527da16c01ca095216e88817
Pratik9113 <117252760+Pratik9113@users.noreply.github.com>
2024-04-14 21:42:54 +0530

    use reducer

--- React_course/todo1/src/App.js
+++ React_course/todo1/src/App.js
@@ -2,22 +2,56 @@ import AppTodo from "./components/AddTodo"
 import AppName from "./components/AppName"
 import './App.css';
 import TodoItems from "./components/TodoItems";
-import {useState} from 'react';
+import {useReducer} from 'react';
 import WelcomeMessage from "./components/WelcomeMessage";
 import {TodoItemsContext} from "./store/todo-items-store"
+const todoItemReducer = (currTodoItems, action) =>{
+  let newTodoItems = currTodoItems;
+  if (action.type === "NEW_ITEM"){
+    newTodoItems = [...currTodoItems,{name : action.payload.itemName,dueDate:action.payload.itemDueDate }]
+  }
+  else if (action.type === "DELETE_ITEM"){
+    newTodoItems = currTodoItems.filter ( item => item.name !== action.payload.itemName)
+  }
+  return newTodoItems;
+}
 function App() {
-  const [todoItems, setTodoItems] = useState([])
+  // const [todoItems, setTodoItems] = useState([])
+
+  const [todoItems, dispatchTodoItems] = useReducer(todoItemReducer, [])
+
+
+
   const addNewItem = (itemName,itemDueDate) => {
-    console.log(`${itemName}`)
+
+    const newItemAction = {
+      type : "NEW_ITEM",
+      payload : {
+        itemName,
+        itemDueDate,
+      }
+    };
+    dispatchTodoItems(newItemAction)
+
+
+    /* console.log(`${itemName}`)
     setTodoItems((currValue)=>{
       const newTodoItem = [...currValue,{name : itemName,dueDate:itemDueDate }]
       return newTodoItem
-    })
+    })*/ 
   }
 
   const deleteItem = (todoItemName) =>{
-    const newTodoItem = todoItems.filter ( item => item.name !== todoItemName)
-    setTodoItems(newTodoItem)
+
+    const deleteItemAction = {
+      type : "DELETE_ITEM",
+      payload : {
+        itemName : todoItemName,
+      }
+    };
+    dispatchTodoItems(deleteItemAction)
+    // const newTodoItem = todoItems.filter ( item => item.name !== todoItemName)
+    // setTodoItems(newTodoItem)
   }
   return (
     <>
