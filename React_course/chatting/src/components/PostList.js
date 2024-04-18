b012fec6f50f639f7856ead59bd7bb1b0a8f51e4
Pratik9113 <117252760+Pratik9113@users.noreply.github.com>
2024-04-18 23:22:35 +0530

    useeffect

--- React_course/chatting/src/components/PostList.js
+++ React_course/chatting/src/components/PostList.js
@@ -1,20 +1,22 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect,  } from 'react'
 import Post from './Post'
 import {PostList as PostListData} from '../store/post-list-store'
 import WelcomeMeassage from './WelcomeMeassage'
 
 const PostList = () => {
   const  {postList,addIntialPosts} = useContext(PostListData)
-  const handleGetPostClick = () =>{  
+  /* useeffect for data fetching or event listerner and aftere every render by default */
+  useEffect(()=>{
     fetch('https://dummyjson.com/posts')
-    .then(res => res.json())
-    .then(data => {
-      addIntialPosts(data.posts)
-    });
-  }
+      .then(res => res.json())
+      .then(data => {
+        addIntialPosts(data.posts)
+      });
+  },[addIntialPosts]);
+
   return (
     <>
-    {postList.length === 0 && <WelcomeMeassage onGetPostClick={handleGetPostClick}/>}
+    {postList.length === 0 && <WelcomeMeassage />}
       {postList.map((post)=> <Post
       key = {post.id} post = {post}
       />)}
