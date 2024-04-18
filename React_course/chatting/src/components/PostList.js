60e7c9f91b628fd1076b099f4ae091c7cff1b438
Pratik9113 <117252760+Pratik9113@users.noreply.github.com>
2024-04-18 23:35:17 +0530

    add loading state

--- React_course/chatting/src/components/PostList.js
+++ React_course/chatting/src/components/PostList.js
@@ -1,23 +1,32 @@
-import React, { useContext, useEffect,  } from 'react'
+import React, { useContext, useEffect, useState,  } from 'react'
 import Post from './Post'
 import {PostList as PostListData} from '../store/post-list-store'
 import WelcomeMeassage from './WelcomeMeassage'
+import Loading from './Loading'
 
 const PostList = () => {
   const  {postList,addIntialPosts} = useContext(PostListData)
+
+
+
+  const  [fetching,setFetching] = useState(false)
   /* useeffect for data fetching or event listerner and aftere every render by default */
   useEffect(()=>{
+    setFetching(true);
     fetch('https://dummyjson.com/posts')
       .then(res => res.json())
       .then(data => {
         addIntialPosts(data.posts)
-      });
-  },[addIntialPosts]);
+        setFetching(false);
 
-  return (
+      });
+  },[]);
+
+  return ( 
     <>
-    {postList.length === 0 && <WelcomeMeassage />}
-      {postList.map((post)=> <Post
+    {fetching && <Loading/>}
+    {!fetching && postList.length === 0 && <WelcomeMeassage />}
+      {!fetching && postList.map((post)=> <Post
       key = {post.id} post = {post}
       />)}
     </>
