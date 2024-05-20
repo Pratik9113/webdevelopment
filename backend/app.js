bced5a64caf655808998a26e3d7be091237c28a1
Pratik9113 <117252760+Pratik9113@users.noreply.github.com>
2024-05-20 17:57:21 +0530

    add jwt

--- backend/app.js
+++ backend/app.js
@@ -3,17 +3,23 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken");
 app.use(cookieParser()); // Notice the parentheses to invoke cookie-parser
 
 app.get('/', (req, res) => {
-    res.cookie("name", "pratik");
+    let token = jwt.sign({email:"pratik.patil9113@gmail.com"}, "secret");
+
+    res.cookie("token",token);
     res.send("done");
 });
 
 app.get('/read', (req, res) => {
-    const nameCookie = req.cookies.name;
-    console.log(nameCookie)
-    res.send(`Reading cookie: ${nameCookie}`);
+    // const nameCookie = req.cookies;
+    // console.log(nameCookie)
+    // res.send(`Reading cookie: ${nameCookie}`);
+
+    const data = jwt.verify(req.cookies.token, "secret");
+    console.log(data);
 });
 
 app.listen(port, () => {
