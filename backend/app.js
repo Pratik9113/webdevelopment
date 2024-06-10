191dc9e7b78c630b4f48f095d23091e6e3ba7c20
Pratik9113 <117252760+Pratik9113@users.noreply.github.com>
2024-06-10 17:39:03 +0530

    add drum project

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,26 @@
 const cookieParser = require('cookie-parser');
 const express = require('express');
+const path = require("path")
 const app = express();
 const port = 3000;
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken");
+app.set("view engine" , "ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')))
+app.use()
 app.use(cookieParser()); // Notice the parentheses to invoke cookie-parser
 
 app.get('/', (req, res) => {
-    let token = jwt.sign({email:"pratik.patil9113@gmail.com"}, "secret");
+    // let token = jwt.sign({email:"pratik.patil9113@gmail.com"}, "secret");
 
-    res.cookie("token",token);
-    res.send("done");
+    // res.cookie("token",token);
+    // res.send("done");
+
+
+    /* 2nd face */
+
 });
 
 app.get('/read', (req, res) => {
@@ -18,8 +28,12 @@ app.get('/read', (req, res) => {
     // console.log(nameCookie)
     // res.send(`Reading cookie: ${nameCookie}`);
 
-    const data = jwt.verify(req.cookies.token, "secret");
-    console.log(data);
+    // const data = jwt.verify(req.cookies.token, "secret");
+    // console.log(data);
+
+
+
+    /* 2nd face */ 
 });
 
 app.listen(port, () => {
